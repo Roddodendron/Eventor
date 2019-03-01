9f1cd73c9f002d647dc53beb1db230d8ec38585e
Andy <Andy@TUXEDO>
2019-03-01 08:09:31 +0100

    --

---
+++ Eventor/Eventor/Form1.vb
@@ -0,0 +1,37 @@
+﻿Imports Microsoft.Win32
+
+Public Class Form1
+    Private Event Appexit2()
+    Public Sub New()
+        AddHandler Application.ApplicationExit, AddressOf AppExit
+        AddHandler Microsoft.Win32.SystemEvents.SessionEnding, AddressOf Eventori
+        ' Dieser Aufruf ist für den Designer erforderlich.
+        InitializeComponent()
+        ' Fügen Sie Initialisierungen nach dem InitializeComponent()-Aufruf hinzu.
+
+    End Sub
+#Region "durch herunterfahren des Systems"
+    Private Sub Eventori(sender As Object, e As SessionEndingEventArgs)
+        MsgBox("herunterfahren des systems")
+    End Sub
+
+    Private Sub AppExit()
+        'Closing by pressing the X
+        Debug.Print("Programm wurde vom Nutzer mit klick auf Y geschlossen")
+    End Sub
+
+#End Region
+
+#Region "eigenes Event"
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        RaiseEvent Appexit2() 'auslösen des Events
+        Application.Exit() 'beenden des Programmes. Löst seltsamerweise nicht das Standardevent aus
+    End Sub
+
+    Private Sub Appexit2_Handler() Handles Me.Appexit2 'Handler für das Event
+        MsgBox("Beenden durch einen anderen Knopf")
+    End Sub
+
+
+#End Region
+End Class
---
+++ Eventor/Eventor/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden 
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die einer Assembly zugeordnet sind.
+
+' Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("Eventor")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Eventor")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird.
+<Assembly: Guid("5f139235-2479-44d9-984c-40f3714e9c36")>
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion 
+'      Buildnummer
+'      Revision
+'
+' Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+' übernehmen, indem Sie "*" eingeben:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
